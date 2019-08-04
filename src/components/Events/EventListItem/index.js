23bace02cd2c8db4f0af7a287fe23020a2aa07d7
Babak Chehraz <bchehraz@csumb.edu>
2019-08-03 19:09:56 -0700

    Added styled component and props validation

--- src/components/Events/EventListItem/index.js
+++ src/components/Events/EventListItem/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const eventItem = props => {
-  <li key={props.eventId} className={styles[`event__li`]}>
+const StyledEventListItem = styled.li`
+  margin: 1rem 0;
+  padding: 1rem;
+  border: 1px solid #663399;
+`;
+
+const EventListItem = props => (
+  <StyledEventListItem eventId={props.eventId}>
     {props.title}
-  </li>
+  </StyledEventListItem>
+);
+
+EventListItem.propTypes = {
+  eventId: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
+
+export default EventListItem;
