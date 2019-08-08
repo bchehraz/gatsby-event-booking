5c4b66b09bdebb4aed8d95c4b1c3fb2733a14525
Babak Chehraz <bchehraz@csumb.edu>
2019-08-07 17:36:21 -0700

    Updated gatsby version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,7 +15,7 @@ const Header = () => (
                 styles[`header__link--home`]
               }`}
             >
-              {`events-app`}
+              {`Gatsby Events`}
             </Link>
           </h1>
           <nav role="main" className={styles[`header__nav`]}>
