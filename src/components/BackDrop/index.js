87affdfb7d4e4e9078641d70e5a5915ca814dc39
Babak Chehraz <bchehraz@csumb.edu>
2019-08-12 15:57:39 -0700

    Added onClick prop to backdrop to allow for a backdrop onClick event

--- src/components/BackDrop/index.js
+++ src/components/BackDrop/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './backdrop.module.css';
 
-const BackDrop = () => (
-  <div className={styles[`backdrop`]}>
+const BackDrop = ({ onClick }) => (
+  <div className={styles[`backdrop`]} onClick={onClick}>
   </div>
 );
+
+BackDrop.propTypes = {
+  onClick: PropTypes.func,
+}
 
 export default BackDrop;
