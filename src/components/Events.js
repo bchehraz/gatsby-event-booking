21672fd541549a3ebb2939e9e1b053461e99f8cd
Babak Chehraz <bchehraz@csumb.edu>
2019-08-12 16:04:01 -0700

    Added spinner to Events page, added functionality to create booking

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,6 +6,7 @@ import Modal from './Modal';
 import BackDrop from './BackDrop';
 import AuthContext from '../context/auth-context';
 import EventsList from './Events/EventsList/';
+import Spinner from './Spinner/';
 
 const CreateEventContainer = styled.div`
   border: 1px solid black;
@@ -21,6 +22,8 @@ class Events extends React.Component {
     date: '',
     creating: false,
     events: [],
+    isLoading: false,
+    selectedEvent: false,
   }
 
   static contextType = AuthContext;
@@ -33,6 +36,8 @@ class Events extends React.Component {
     // this.descriptionRef = React.createRef();
 
     this.handleUpdate = this.handleUpdate.bind(this);
+    this.onConfirmCreateEvent = this.onConfirmCreateEvent.bind(this);
+    this.bookEventHandler = this.bookEventHandler.bind(this);
   }
 
   componentDidMount() {
@@ -40,11 +45,11 @@ class Events extends React.Component {
   }
 
   startCreateEventHandler = () => {
-    this.setState({ ...this.state, creating: true });
+    this.setState({ creating: true });
   }
 
-  onCancelCreateEvent = () => {
-    this.setState({ ...this.state, creating: false });
+  onCancelAction = () => {
+    this.setState({ creating: false, selectedEvent: null, });
   }
 
   onConfirmCreateEvent = () => {
@@ -71,7 +76,6 @@ class Events extends React.Component {
     console.log(event);
 
     //prepare the graphql api request
-
     const requestBody = {
       query: `
         mutation {
@@ -86,10 +90,6 @@ class Events extends React.Component {
             description
             price
             date
-            creator {
-              _id
-              email
-            }
           }
         }
       `
@@ -121,13 +121,28 @@ class Events extends React.Component {
       console.log("Server Output >> ");
       console.log(resData.data);
 
-      this.fetchEvents();
+      this.setState(prevState => {
+        const updatedEvents = [...prevState.events];
+        const { title, description, price, date } = resData.data.createEvent;
+        updatedEvents.push({
+          _id: this.context.userId,
+          title: title,
+          description: description,
+          price: price,
+          date: date,
+          creator: {
+            _id: this.context.userId
+          },
+        });
+        return { events: updatedEvents };
+      });
     }).catch(err => {
       console.log(err);
     });
   }
 
   fetchEvents() {
+    this.setState({ isLoading: true });
     const requestBody = {
       query: `
         query {
@@ -164,9 +179,11 @@ class Events extends React.Component {
       console.log(resData.data);
 
       const events = resData.data.events;
-      this.setState({ events: events });
+
+      this.setState({ events: events, isLoading: false });
     }).catch(err => {
       console.log(err);
+      this.setState({ isLoading: false });
     });
   }
 
@@ -176,61 +193,121 @@ class Events extends React.Component {
     });
   }
 
+  showDetailHandler = eventId => {
+    this.setState(prevState => {
+      const selectedEvent = prevState.events.find(e => e._id === eventId);
+      return { selectedEvent: selectedEvent };
+    })
+  }
+
+  bookEventHandler() {
+    if (!this.context.token) {
+      this.setState({ selectedEvent: null });
+      return;
+    }
+
+    const requestBody = {
+      query: `
+        mutation {
+          bookEvent(eventId: "${this.state.selectedEvent._id}") {
+            _id
+            createdAt
+            updatedAt
+          }
+        }
+      `
+    };
+    console.log("Preparing Server Request >> Request Body Complete.");
+    const { token } = this.context;
+    fetch('https://graphql-event-booking.herokuapp.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    }).then(res => {
+      if(res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    }).then(resData => { // successful login or sign up
+      console.log("Server Output >> Book Event Mutation Successful");
+      console.log("Server Output >> ");
+      console.log(resData.data);
+      //console.log(resData);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <View title="Events">
-      {this.state.creating && <BackDrop />}
+      {(this.state.creating || this.state.selectedEvent) && (
+        <BackDrop onClick={this.onCancelAction} />
+      )}
       {this.state.creating &&
         <Modal
           title="Add Event"
           canCancel
           canConfirm
-          onCancel={this.onCancelCreateEvent}
+          onCancel={this.onCancelAction}
           onConfirm={this.onConfirmCreateEvent}
+          confirmText="Confirm"
         >
-        <form className={formStyles.form}>
-          <label className={formStyles[`form__label`]}>
-            Title
-            <input
-              className={formStyles[`form__input`]}
-              type="text"
-              // ref={this.titleRef}
-              name="title"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <label className={formStyles[`form__label`]}>
-            Price
-            <input
-              className={formStyles[`form__input`]}
-              type="number"
-              // ref={this.priceRef}
-              name="price"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <label className={formStyles[`form__label`]}>
-            Date
-            <input
-              className={formStyles[`form__input`]}
-              type="datetime-local"
-              // ref={this.dateRef}
-              name="date"
-              onChange={this.handleUpdate}
-            />
-          </label>
-          <label className={formStyles[`form__label`]}>
-            Description
-            <textarea
-              className={formStyles[`form__textarea`]}
-              rows={4}
-              // ref={this.descriptionRef}
-              name="description"
-              onChange={this.handleUpdate}
-            />
-          </label>
-        </form>
-      </Modal>
+          <form className={formStyles.form}>
+            <label className={formStyles[`form__label`]}>
+              Title
+              <input
+                className={formStyles[`form__input`]}
+                type="text"
+                name="title"
+                onChange={this.handleUpdate}
+              />
+            </label>
+            <label className={formStyles[`form__label`]}>
+              Price
+              <input
+                className={formStyles[`form__input`]}
+                type="number"
+                name="price"
+                onChange={this.handleUpdate}
+              />
+            </label>
+            <label className={formStyles[`form__label`]}>
+              Date
+              <input
+                className={formStyles[`form__input`]}
+                type="datetime-local"
+                name="date"
+                onChange={this.handleUpdate}
+              />
+            </label>
+            <label className={formStyles[`form__label`]}>
+              Description
+              <textarea
+                className={formStyles[`form__textarea`]}
+                rows={4}
+                name="description"
+                onChange={this.handleUpdate}
+              />
+            </label>
+          </form>
+        </Modal>
+      }
+      {this.state.selectedEvent &&
+        <Modal
+          title={this.state.selectedEvent.title}
+          canCancel
+          canConfirm
+          onCancel={this.onCancelAction}
+          onConfirm={this.bookEventHandler}
+          confirmText={this.context.token ? "Book This Event" : "Confirm"}
+        >
+          <h2>${this.state.selectedEvent.price} - {new Date(this.state.selectedEvent.date).toLocaleDateString()}</h2>
+          <p>{this.state.selectedEvent.description}</p>
+        </Modal>
       }
         {this.context.token && (
           <CreateEventContainer>
@@ -240,13 +317,20 @@ class Events extends React.Component {
                 className={formStyles[`form__button`]}
                 onClick={this.startCreateEventHandler}
               >
-                Create Event
+                {`Create Event`}
               </button>
             </div>
           </CreateEventContainer>
         )}
 
-        <EventsList events={this.state.events} />
+        {this.state.isLoading
+          ? <Spinner />
+          : <EventsList
+              events={this.state.events}
+              authUserId={this.context.userId + ""}
+              onViewDetail={this.showDetailHandler}
+            />
+        }
       </View>
     );
   }
