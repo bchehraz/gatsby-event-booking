06306a133f6ea25d56649d9822492f9d2dc0dd5b
Babak Chehraz <bchehraz@csumb.edu>
2019-08-13 18:35:37 -0700

    Added cancel booking mutation, refactored code

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import View from './View';
 import Spinner from './Spinner';
+import BookingList from './Bookings/BookingList';
 import AuthContext from '../context/auth-context';
 
 class Bookings extends React.Component {
@@ -67,21 +68,64 @@ class Bookings extends React.Component {
     });
   }
 
+  cancelBookingHandler = bookingId => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+        mutation CancelBooking($id: ID!) {
+          cancelBooking(bookingId: $id) {
+            _id
+            title
+          }
+        }
+      `,
+      variables: {
+        id: bookingId
+      }
+    };
+    console.log("Preparing Server Request >> Request Body Complete.");
+
+    const { token } = this.context;
+
+    fetch('https://graphql-event-booking.herokuapp.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token,
+      }
+    }).then(res => {
+      if(res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!');
+      }
+      return res.json();
+    }).then(resData => { // successful login or sign up
+      console.log("Server Output >> Cancel Booking Mutation Successful");
+      console.log("Server Output >> ");
+      console.log(resData.data);
+
+      this.setState(prevState => {
+        const updatedBookings = prevState.bookings.filter(booking => {
+          return booking._id !== bookingId;
+        });
+
+        return { bookings: updatedBookings, isLoading: false };
+      });
+    }).catch(err => {
+      console.log(err);
+      this.setState({ isLoading: false });
+    });
+  }
+
   render() {
     const { bookings, isLoading } = this.state;
     return (
       <View title="Your Bookings">
         {isLoading ? <Spinner /> : (
-          <ul>
-            {bookings.map(booking => {
-              return (
-                <li key={booking._id}>
-                  {booking.event.title} - {' '}
-                  {new Date(booking.event.date).toLocaleDateString()}
-                </li>
-              );
-            })}
-          </ul>
+          <BookingList
+            bookings={bookings}
+            onCancelBooking={this.cancelBookingHandler}
+          />
         )}
       </View>
     );
