447f570fb92fcf6968dae658452fec91190d5a6f
Babak Chehraz <bchehraz@csumb.edu>
2019-08-20 00:20:37 -0700

    Removed unnecessary redirect from the home page, - Made a check in componentDidMount for if a user is already logged in and it will set the context accordingly

--- src/pages/app.js
+++ src/pages/app.js
@@ -6,11 +6,12 @@ import Login from '../components/Login';
 import PrivateRoute from '../components/PrivateRoute';
 
 //import EventsPage from './components/events';
-// import Home from '../components/Home';
+import Home from '../components/Home';
 import Events from '../components/Events';
 import Bookings from '../components/Bookings';
 import Status from '../components/Status';
 import AuthContext from '../context/auth-context';
+import { isLoggedIn, onLoginSuccess } from '../utils/auth.js';
 
 class App extends React.Component {
   state = {
@@ -18,8 +19,14 @@ class App extends React.Component {
     userId: null,
   }
 
+  componentDidMount() {
+    const { token, userId } = isLoggedIn();
+    this.setState({ token, userId });
+  }
+
   login = (token, userId, tokenExpiration) => {
-    this.setState({ token: token, userId: userId, tokenExpiration: tokenExpiration });
+    this.setState({ token, userId, tokenExpiration });
+    onLoginSuccess(token, userId, tokenExpiration);
   }
 
   logout = () => {
@@ -40,12 +47,12 @@ class App extends React.Component {
           <Status />
           <Router>
             {this.state.token && <Redirect from="/app/login" to="/app/events" exact noThrow />}
-            {this.state.token && <Redirect from="/" to="/app/events" exact noThrow />}
 
-            <Events path="/app/events" />
             <PrivateRoute path="/app/bookings" component={Bookings} />
+            <Events path="/app/events" />
             <Login signUp={false} path="/app/login" />
             <Login signUp path="/app/sign-up" />
+            <Home path="/" exact />
           </Router>
         </Layout>
       </AuthContext.Provider>
