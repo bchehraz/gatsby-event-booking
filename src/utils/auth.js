3d979334c31b52cfccabd997351adaed37961629
Babak Chehraz <bchehraz@csumb.edu>
2019-08-20 00:21:16 -0700

    Made some changes

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -9,89 +9,27 @@ const setUser = user => {
   window.localStorage.user = JSON.stringify(user);
 }
 
-/* * * * * *
-  LOG IN and SIGN UP in one function
-* * * * * */
-
-export const handleLogin = ({ email = "", password = "", signUp }, context) => {
-  if (!isBrowser) return false;
-
-  if (email.trim().length === 0 || password.trim().length === 0) {
-    return false;
-  }
-
-  let requestBody = {
-    query: `
-      query Login {
-        login(
-          email: "${email}",
-          password: "${password}"
-        ) {
-          userId
-          token
-          tokenExpiration
-        }
-      }
-    `
-  };
-  if (signUp) {
-    requestBody = {
-      query: `
-        mutation SignUp {
-          createUser(userInput: {
-            email: "${email}",
-            password: "${password}"
-          }) {
-            _id
-            email
-          }
-        }
-      `
-    };
-  }
-  console.log("[Loading] Request Body Complete. Sign up = " + signUp);
-  fetch('http://localhost:3000/graphql', {
-    method: 'POST',
-    body: JSON.stringify(requestBody),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(res => {
-    if(res.status !== 200 && res.status !== 201) {
-      return false;
-      throw new Error('Failed!');
-    }
-    return res.json();
-  }).then(resData => { // successful login or sign up
-    console.log(resData);
-    console.log("RESULT DATA>>");
-    /*console.log(resData.data.login.token);
-    console.log(resData.data.login.userId);
-    console.log(resData.data.login.tokenExpiration);*/
-    console.log(resData.data);
-
-
-    if (resData.data.login) {
-      context.login(resData.data.login.token, resData.data.login.userId, resData.data.login.tokenExpiration);
-      return true;
-    } else if (resData.data.createUser) {
-      //
-    }
-
-  }).catch(err => {
-    console.log(err);
-  });
-
-
-
-  return false;
+export const getToken = () => {
+  return getUser().token;
 }
 
 export const isLoggedIn = () => {
   if (!isBrowser) return false;
 
-  const user = getUser();
-  return !!user.email;
+  const { token, userId } = getUser();
+  if (!token) {
+    return false;
+  }
+  return { token, userId };
+}
+
+export const onLoginSuccess = (token, userId, tokenExpiration) => {
+  console.log(`Login Successful`);
+  return setUser({
+    token,
+    userId,
+    tokenExpiration
+  });
 }
 
 export const getCurrentUser = () => isBrowser && getUser();
