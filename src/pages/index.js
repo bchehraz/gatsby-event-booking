a971a0a99cf7f351e5f23021219a2d437b8019f2
Babak Chehraz <bchehraz@csumb.edu>
2019-08-20 00:21:00 -0700

    Moved App into IndexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 
-// import App from './app.js';
-import Home from '../components/Home';
+import App from './app.js';
+// import Home from '../components/Home';
 import Layout from '../components/Layout';
-import Status from '../components/Status';
+// import Status from '../components/Status';
+// import AuthContext from '../context/auth-context';
 
-const IndexPage = () => (
-  // <App />
-  <Layout>
-    <Status />
-    <Home />
-  </Layout>
-);
+const IndexPage = () => <App />
 
 export default IndexPage;
