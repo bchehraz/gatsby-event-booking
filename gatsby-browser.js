b93a7d683f6135e82fb464ab846ab45a3545c9b9
Babak Chehraz <bchehraz@csumb.edu>
2019-08-22 23:14:37 -0700

    Added a message to gatsby-browser which will prompt user to refresh to see updated web page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `GatsbyEvents has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
